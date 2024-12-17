e755164f8bc6a886c5e76eb4a077c8c216938b0f
D-Mayerfeld <mayerfeld.dassi@gmail.com>
2024-12-17 13:08:55 -0500

    Var_args setup

--- function.c
+++ function.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+void my_prinf(int string, ...){
+  // declare a va list to hold the arguments. va_list is the type used to store the variable arguments.
+  va_list args;
+
+  // Initialize the argument list
+  va_start(args, 0); // the last value in the list is 0, indicating the end of the list
+
+  //va_args(args, type) retrieves the next argument
+
+  va_end(args); //clean up the va_list
+}
+
 
 int main() {
 }
+
+
