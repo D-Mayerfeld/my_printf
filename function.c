a51e72c774d1923c20b8f4287071c2e40cb26463
D-Mayerfeld <mayerfeld.dassi@gmail.com>
2024-12-28 19:43:43 -0500

    Update function.c

--- function.c
+++ function.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 
 void my_prinf(char *input_string, ...){
-  char *string = input_string; //The variable "string" now points to the first character in the string
+  char * string = input_string; //The variable "string" now points to the first character in the string
   
   // declare a va_list to hold the arguments. va_list is the type used to store the variable arguments.
   va_list args;
@@ -12,14 +12,22 @@ void my_prinf(char *input_string, ...){
 
   //Loop through the string and write to stdout
   char current; //initializes a variable to store the current character in from the string
-  for(string; *string != '\0'; string++){
-    current = *string;
-    putchar(current);
+  while(*string != '\0'){
+    current = *string; //current points to that is stored in the memory location
+
+    // While we are up to a regular character, print the character to the stdout
+    if (current != '%'){
+      putchar(current);
+      string ++; //increments the pointer
+    }
+
+    //add code here to save the modifier values -- what each is -- and pass into function dependent on data type
+    // update the string pointer accordingly
+     
+    
   }
 
-
-
-  
+ 
   //va_arg(args, type) retrieves the next argument of that type in the list
 
   va_end(args); //clean up the va_list
