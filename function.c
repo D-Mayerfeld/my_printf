58a07b7cc82b23d0242385e466fafa13e592695d
D-Mayerfeld <mayerfeld.dassi@gmail.com>
2024-12-28 19:32:32 -0500

    set up loop with putchar

--- function.c
+++ function.c
@@ -10,6 +10,16 @@ void my_prinf(char *input_string, ...){
   // Initialize the argument list
   va_start(args, 0); // the last value in the list is 0, indicating the end of the list
 
+  //Loop through the string and write to stdout
+  char current; //initializes a variable to store the current character in from the string
+  for(string; *string != '\0'; string++){
+    current = *string;
+    putchar(current);
+  }
+
+
+
+  
   //va_arg(args, type) retrieves the next argument of that type in the list
 
   va_end(args); //clean up the va_list
@@ -24,7 +34,20 @@ int main() {
 
 
 /* EXTRA CODE
-  // check va_list is working
+
+// check string is working and iterate through it
+void iterating_string(char *input_string){
+  char *string = input_string; //The variable "string" now points to the first character in the string
+  char current; //initializes a variable to store the current character in from the string
+  for(string; *string != '\0'; string++){
+    current = *string;
+    putchar(current);
+  }
+}
+
+
+
+// check va_list is working
   while (1) {
     int value = va_arg(args, int);
     if (value == 0){
