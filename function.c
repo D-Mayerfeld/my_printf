10efd5bffd3f0ee7642ec70ff5b852df681a6707
D-Mayerfeld <mayerfeld.dassi@gmail.com>
2025-01-22 16:20:52 -0500

    COMMENTS

--- function.c
+++ function.c
@@ -6,7 +6,7 @@
 
 //make sure that throws error if not one of the 4 data types
 //make sure throws error if any mistake with any of the specifiers
-//make sure throws error if tries to retireve from va_list and hits 0 (end of the list) i.e. there were not enough arguments specified by their function call
+//make sure throws error if tries to retireve from va_list and hits 0 (end of the list) i.e. there were not enough arguments specified by their function call -- BOTH IN GENERAL FUNCTION IF TOO MANY %s AND ALSO IN DECIMAL FUNCTION IF USE * TO RETRIEVE SECOND AREGUMENT
 //make sure that if the general function returns with things still on va_list to throw a warning message
 //indicate that I made the choice that if an unknown specifier is used it is ignored and no error message is thrown back
 // make sure negative numbers naturally print with a negative sign
